8d24b99f37a6612e11c2a571f9f5863bc096b198
good1luck21 <good1luck21@gmail.com>
2018-01-25 14:15:49 +0900

    作業一旦中断のためコミット。 ・Bootstrapを追加。 ・壊れていたsharedフォルダを追加。 ・追加したsharedにfooter,headerを追加。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users=User.find_by(id: params[:id])
+    @user = User.find_by(id: params[:id])
   end
 
   def create
-    @user=User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       redirect_to("/users/#{@user.id}/index")
     else
