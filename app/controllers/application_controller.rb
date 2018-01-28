9c188f20b1c76f5229bdbb64692f4b37206f0976
岡崎竜貴 <okazakitatsuki@okazakiryuuki-no-MacBook-Air.local>
2018-01-28 14:09:39 +0900

    html等変更しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def top
+  end
+
+  def contact
+  end
+
+  def signup
+  end
+
+  def link
+  end
+
+  def signin
+  end
+  
 end
